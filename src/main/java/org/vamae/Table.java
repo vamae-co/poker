6d8b97f1b014d398c1c6b567342918c0f2174e51
Vladyslav <v1adem@ukr.net>
2024-02-29 15:15:45 +0200

    Implemented StartState class

---
+++ src/main/java/org/vamae/Table.java
@@ -0,0 +1,30 @@
+package org.vamae;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Table {
+    @Getter
+    private final List<Player> players;
+    private GameState state;
+
+    public Table() {
+        players = new ArrayList<>();
+        state = new WaitingState(this);
+    }
+
+    public Optional<Player> join() {
+        return state.join();
+    }
+
+    public boolean start() {
+        return state.start();
+    }
+
+    protected void changeState(GameState newState) {
+        state = newState;
+    }
+}
