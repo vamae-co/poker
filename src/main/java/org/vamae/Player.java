6124a309ca51ea91a3e26cbbc71ce321cbc31810
Makarov Andrii <makarov.andrii.10@gmail.com>
2024-03-04 13:57:31 +0200

    update Player

--- src/main/java/org/vamae/Player.java
+++ src/main/java/org/vamae/Player.java
@@ -1,15 +1,24 @@
 package org.vamae;
 
-import org.vamae.Card;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
+    private final int id;
+    private final Table table;
     private List<Card> hand;
+    @Getter
+    @Setter
+    private boolean isFolded;
 
-    public Player(){
+    public Player(int id, Table table){
+        this.id = id;
+        this.table = table;
         hand = new ArrayList<>();
+        isFolded = false;
     }
 
     public void take(Card card) {
