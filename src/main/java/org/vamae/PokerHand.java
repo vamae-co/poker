fc908ae91f493355e5ea879fcc9bc8684b85b0a7
Makarov Andrii <makarov.andrii.10@gmail.com>
2024-03-06 13:50:13 +0200

    add PokerHand and Combination

---
+++ src/main/java/org/vamae/PokerHand.java
@@ -0,0 +1,12 @@
+package org.vamae;
+
+import lombok.AllArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+public class PokerHand {
+    public Combination combination;
+    public List<Card> cards;
+}
