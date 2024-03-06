b3e894da95cb635c5864111b7d17eb57d49b2719
Makarov Andrii <makarov.andrii.10@gmail.com>
2024-03-06 16:59:01 +0200

    fix bugs/refactor Poker

--- src/main/java/org/vamae/Poker.java
+++ src/main/java/org/vamae/Poker.java
@@ -5,71 +5,52 @@ import java.util.stream.Collectors;
 
 public class Poker {
     public static PokerHand check(List<Card> tableCards, List<Card> playerCards) {
-        List<Card> allCards = new ArrayList<>(tableCards);
-        allCards.addAll(playerCards);
-
+        List<Card> allCards = combineCards(tableCards, playerCards);
         allCards.sort(Comparator.comparing(Card::rank));
 
-        Map<Rank, Long> rankFrequency = allCards
+        Map<Rank, Long> rankFrequency = getRankFrequency(allCards);
+
+        return determineBestHand(rankFrequency, allCards);
+    }
+
+    private static List<Card> combineCards(List<Card> tableCards, List<Card> playerCards) {
+        List<Card> allCards = new ArrayList<>(tableCards);
+        allCards.addAll(playerCards);
+        return allCards;
+    }
+
+    private static Map<Rank, Long> getRankFrequency(List<Card> allCards) {
+        return allCards
                 .stream()
                 .collect(Collectors.groupingBy(Card::rank, Collectors.counting()));
+    }
 
-        List<Card> bestComboCards = new ArrayList<>();
+    private static PokerHand determineBestHand(Map<Rank, Long> rankFrequency, List<Card> allCards) {
+        List<Card> bestComboCards = new ArrayList<>(List.of(allCards.getLast()));
         Combination bestHand = Combination.HIGH_CARD;
-        bestComboCards.add(allCards.getLast());
 
-        boolean hasPair = false;
-        boolean hasThreeOfAKind = false;
-
-        for (Map.Entry<Rank, Long> entry : rankFrequency.entrySet()) {
-            if (entry.getValue() == 2) {
-                if (hasThreeOfAKind) {
-                    bestHand = Combination.FULL_HOUSE;
-                    bestComboCards.addAll(getAllCardsWithRankFromList(allCards, entry.getKey()));
-                    break;
-                } else if (hasPair) {
-                    bestHand = Combination.TWO_PAIR;
-                    bestComboCards.addAll(getAllCardsWithRankFromList(allCards, entry.getKey()));
-                } else {
-                    hasPair = true;
-                    bestHand = Combination.ONE_PAIR;
-                    bestComboCards = allCards
-                            .stream()
-                            .filter(card -> card.rank() == entry.getKey())
-                            .collect(Collectors.toList());
-                }
-            } else if (entry.getValue() == 3) {
-                if (hasThreeOfAKind) {
-                    bestHand = Combination.FULL_HOUSE;
-                    bestComboCards.addAll(getAllCardsWithRankFromList(allCards, entry.getKey()));
-                    bestComboCards.removeFirst();
-                    break;
-                }
-                if (hasPair) {
-                    bestHand = Combination.FULL_HOUSE;
-                    bestComboCards.addAll(getAllCardsWithRankFromList(allCards, entry.getKey()));
-                    break;
-                } else {
-                    hasThreeOfAKind = true;
-                    bestHand = Combination.THREE_OF_A_KIND;
-                    bestComboCards = allCards
-                            .stream()
-                            .filter(card -> card.rank() == entry.getKey())
-                            .collect(Collectors.toList());
-                }
-            } else if (entry.getValue() == 4) {
-                bestHand = Combination.FOUR_OF_A_KIND;
-                bestComboCards = allCards
+        if (rankFrequency.containsValue(4L)) {
+            bestHand = Combination.FOUR_OF_A_KIND;
+            bestComboCards = determineFourOfAKind(rankFrequency, allCards);
+        } else if (hasFullHouse(rankFrequency)) {
+            bestHand = Combination.FULL_HOUSE;
+            bestComboCards = determineFullHouse(rankFrequency, allCards);
+        } else if (rankFrequency.containsValue(3L)) {
+            bestHand = Combination.THREE_OF_A_KIND;
+            bestComboCards = determineThreeOfAKind(rankFrequency, allCards);
+        } else if (hasTwoPair(rankFrequency)) {
+            bestHand = Combination.TWO_PAIR;
+            bestComboCards = determineTwoPair(rankFrequency, allCards);
+        } else if (
+                rankFrequency.values()
                         .stream()
-                        .filter(card -> card.rank() == entry.getKey())
-                        .collect(Collectors.toList());
-                break;
-            }
+                        .anyMatch(count -> count == 2)
+        ) {
+            bestHand = Combination.ONE_PAIR;
+            bestComboCards = determineOnePair(rankFrequency, allCards);
         }
 
         Optional<List<Card>> straight = getHighestStraight(allCards);
-        Optional<List<Card>> flush = getFlushCards(allCards);
-
         if (straight.isPresent()) {
             if (getFlushCards(straight.get()).isPresent()) {
                 bestHand = Combination.STRAIGHT_FLUSH;
@@ -77,12 +58,109 @@ public class Poker {
                 bestHand = Combination.STRAIGHT;
             }
             bestComboCards = straight.get();
-        } else if (flush.isPresent()) {
+        } else if (getFlushCards(allCards).isPresent()) {
             bestHand = Combination.FLUSH;
-            bestComboCards = flush.get();
+            bestComboCards = getFlushCards(allCards).get();
         }
 
         return new PokerHand(bestHand, bestComboCards);
+    }
+
+    private static List<Card> determineFourOfAKind(Map<Rank, Long> rankFrequency, List<Card> allCards) {
+        Rank rank = rankFrequency.entrySet()
+                .stream()
+                .filter(entry -> entry.getValue() == 4)
+                .map(Map.Entry::getKey)
+                .findAny()
+                .orElse(null);
+        return allCards
+                .stream()
+                .filter(card -> card.rank() == rank)
+                .toList();
+    }
+
+    private static boolean hasFullHouse(Map<Rank, Long> rankFrequency) {
+        return rankFrequency.containsValue(3L) && rankFrequency.containsValue(2L)
+                || rankFrequency.values()
+                .stream()
+                .filter(count -> count == 3)
+                .count() > 1;
+    }
+
+    private static List<Card> determineFullHouse(Map<Rank, Long> rankFrequency, List<Card> allCards) {
+        List<Rank> ranks = rankFrequency.entrySet()
+                .stream()
+                .filter(entry -> entry.getValue() == 3 || entry.getValue() == 2)
+                .sorted(
+                        Comparator
+                                .comparing(entry -> ((Map.Entry<Rank, Long>) entry).getValue())
+                                .thenComparing(entry -> ((Map.Entry<Rank, Long>) entry).getKey())
+                )
+                .map(Map.Entry::getKey)
+                .toList();
+        List<Card> bestComboCards = allCards
+                .stream()
+                .filter(card -> card.rank() == ranks.getLast())
+                .collect(Collectors.toList());
+        bestComboCards.addAll(allCards
+                .stream()
+                .filter(card -> card.rank() == ranks.get(ranks.size() - 2))
+                .limit(2)
+                .toList());
+        return bestComboCards;
+    }
+
+    private static List<Card> determineThreeOfAKind(Map<Rank, Long> rankFrequency, List<Card> allCards) {
+        Rank rank = rankFrequency.entrySet()
+                .stream()
+                .filter(entry -> entry.getValue() == 3)
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElse(null);
+        return allCards
+                .stream()
+                .filter(card -> card.rank() == rank)
+                .collect(Collectors.toList());
+    }
+
+    private static boolean hasTwoPair(Map<Rank, Long> rankFrequency) {
+        return rankFrequency.values()
+                .stream()
+                .filter(count -> count == 2)
+                .count() > 1;
+    }
+
+    private static List<Card> determineTwoPair(Map<Rank, Long> rankFrequency, List<Card> allCards) {
+        List<Rank> ranks = rankFrequency.entrySet()
+                .stream()
+                .filter(entry -> entry.getValue() == 2)
+                .map(Map.Entry::getKey)
+                .sorted()
+                .toList();
+        List<Card> bestComboCards = allCards
+                .stream()
+                .filter(card -> card.rank() == ranks.getLast())
+                .collect(Collectors.toList());
+        bestComboCards.addAll(
+                allCards
+                        .stream()
+                        .filter(card -> card.rank() == ranks.get(ranks.size() - 2))
+                        .toList()
+        );
+        return bestComboCards;
+    }
+
+    private static List<Card> determineOnePair(Map<Rank, Long> rankFrequency, List<Card> allCards) {
+        Rank rank = rankFrequency.entrySet()
+                .stream()
+                .filter(entry -> entry.getValue() == 2)
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElse(null);
+        return allCards
+                .stream()
+                .filter(card -> card.rank() == rank)
+                .collect(Collectors.toList());
     }
 
     private static List<Card> getAllCardsWithRankFromList(List<Card> allCards, Rank rank) {
@@ -118,6 +196,14 @@ public class Poker {
         return suitMap.values()
                 .stream()
                 .filter(list -> list.size() >= 5)
+                .map(
+                        list -> list
+                                .stream()
+                                .sorted(Comparator.comparing(Card::rank)
+                                        .reversed())
+                                .limit(5)
+                                .toList()
+                )
                 .findAny();
     }
 }
